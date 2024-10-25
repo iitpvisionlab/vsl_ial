3c1ed309b4470d1afa2eab883340bf81a3049335
Arseniy Terekhin <senyai@gmail.com>
2024-10-25 19:04:47 +0300

    feat: precompute LIN_RGB_MATRIX_INV
    closes: #1

--- vsl_ial/cs/linrgb.py
+++ vsl_ial/cs/linrgb.py
@@ -6,12 +6,21 @@ import warnings
 
 LIN_RGB_MATRIX = np.asarray(
     (
-        (3.2404542, -1.5371385, -0.4985314),
-        (-0.9692660, 1.8760108, 0.0415560),
-        (0.0556434, -0.2040259, 1.0572252),
+        (3.2404542, -0.9692660, 0.0556434),
+        (-1.5371385, 1.8760108, -0.2040259),
+        (-0.4985314, 0.0415560, 1.0572252),
     ),
     dtype=np.float64,
-).T
+)
+
+LIN_RGB_MATRIX_INV = np.asarray(
+    (
+        (0.4124564, 0.2126729, 0.0193339),
+        (0.3575761, 0.7151522, 0.1191920),
+        (0.1804375, 0.0721750, 0.9503041),
+    ),
+    dtype=np.float64,
+)
 
 
 class linRGB(CS):
@@ -25,8 +34,10 @@ class linRGB(CS):
 
     def _from_sRGB(self, src: CS, color: FArray) -> FArray:
         if color.min() < 0 or color.max() > 1:
+            # this warning is usually harmless
             warnings.warn(
-                f"sRGB range should be in [0, 1] not [{color.min()}, {color.max()}]"
+                f"sRGB range should be in [0, 1] not "
+                f"[{color.min()}, {color.max()}]"
             )
 
         thres = 12.92 * 0.0031308
@@ -50,4 +61,4 @@ class linRGB(CS):
         return color
 
     def _to_XYZ(self, src: CS, color: FArray) -> FArray:
-        return color @ np.linalg.inv(LIN_RGB_MATRIX)
+        return color @ LIN_RGB_MATRIX_INV
