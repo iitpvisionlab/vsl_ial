cb4eb56042bdf6bfbc1d501d1426afc4d0e0739c
Arseniy Terekhin <senyai@gmail.com>
2025-02-13 14:58:28 +0300

    refactor: transpose matrices to remove `.T`

--- vsl_ial/cs/lms.py
+++ vsl_ial/cs/lms.py
@@ -11,23 +11,23 @@ class LMS(CS):
 
     VON_KRIES_M = np.asarray(
         (
-            (0.40024, 0.70760, -0.08081),
-            (-0.2263, 1.16532, 0.04570),
-            (0.0, 0.0, 0.9182200),
+            (0.40024, -0.2263, 0.0),
+            (0.70760, 1.16532, 0.0),
+            (-0.08081, 0.04570, 0.9182200),
         ),
         dtype=np.float64,
     )
     VON_KRIES_M_INV = np.asarray(
         (
-            (1.8599364, -1.1293816, 0.2198974),
-            (0.3611914, 0.6388125, -0.0000064),
-            (0.0, 0.0, 1.0890636),
+            (1.8599364, 0.3611914, 0.0),
+            (-1.1293816, 0.6388125, 0.0),
+            (0.2198974, -0.0000064, 1.0890636),
         ),
         dtype=np.float64,
     )
 
     def from_XYZ(self, src: CS, color: FArray) -> FArray:
-        return color @ self.VON_KRIES_M.T
+        return np.tensordot(color, self.VON_KRIES_M, axes=1)
 
     def to_XYZ(self, src: CS, color: FArray) -> FArray:
-        return color @ self.VON_KRIES_M_INV.T
+        return np.tensordot(color, self.VON_KRIES_M_INV, axes=1)
